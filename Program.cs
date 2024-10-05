dbb95f264a85419cd2e4236b095f3591c8abb4bc
dvinh3406 <pdv206@gmail.com>
2024-10-05 15:03:13 +0700

    du an hoan thien

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap chieu rong hinh chu nhat");
+        float width = float.Parse(Console.ReadLine());
+        Console.WriteLine("nhap chieu cao hinh chu nhat");
+        float height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine ("chu vi hinh chu nhat la {0}",(width+height)*2);
+        Console.WriteLine ($"dien tich hinh chu nhat la {width+height}");
+    }
+}
